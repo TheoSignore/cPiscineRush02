f1ad91b6cd5dffaf205b65c5f4aacb8967f4132f
Theo SIGNORE <tsignore@e2r2p16.42.fr>
2020-07-19 20:10:58 +0200

    lib synch - use tsignore/ft.h

--- tsignore/lib.h
+++ tsignore/lib.h
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   lib.h                                              :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 17:19:41 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/19 18:00:48 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/19 20:09:53 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIB_H
-# define LIB_H
+#ifndef FT_H
+# define FT_H
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <libgen.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
@@ -32,5 +35,15 @@ void	ft_unit(char *nbr, char **dict);
 void	ft_say_order(char *nbr, char **dict);
 void	ft_say(char *nbr, char **dict);
 void	ft_say_three(char *nbr, char **dict);
+int		split_tab(char *str, char **av);
+char	*fill_splited(char *str, int i, int alloc);
+int		nbr_mots(char *str, char *charset, int len);
+int		is_inchar(char cmp, char *charset);
+char	**ft_split(char *str, char *charset);
+char	**ft_splited(char **splited, char *str, char *charset, int len);
+int		ft_error(int ac, char **av);
+int		ft_putstint(char *str);
+int		check_valid_dico(char *tab);
+int		is_nu(char c);
 
 #endif
