25d1b3fe02b999f1e5bdbd116fdf7d787c3f1ad6
Valentin LOIRET <vloiret@e2r2p10.42.fr>
2020-07-19 23:28:22 +0200

    check_tab normalisee

--- ex00/errordict.c
+++ ex00/errordict.c
@@ -6,7 +6,7 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/19 22:53:58 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/19 23:01:44 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/19 23:27:27 by vloiret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,20 @@ int		check_first(char c)
 		return (1);
 }
 
+int		ft_check_tab_bis(int j, int i, char **tab)
+{
+	while (tab[i][++j])
+	{
+		if (tab[i][j] != ' ' && tab[i][j] > 31 && tab[i][j] < 127)
+			return (0);
+	}
+	return (1);
+}
+
 int		check_tab(char **tab)
 {
 	int i;
 	int j;
-	int check;
 
 	i = -1;
 	while (tab[++i])
@@ -36,13 +45,7 @@ int		check_tab(char **tab)
 				return (1);
 		}
 		j++;
-		check = 1;
-		while (tab[i][++j])
-		{
-			if (tab[i][j] != ' ' && tab[i][j] > 31 && tab[i][j] < 127)
-				check = 0;
-		}
-		if (check == 1)
+		if (ft_check_tab_bis(j, i, tab))
 			return (1);
 	}
 	return (0);
